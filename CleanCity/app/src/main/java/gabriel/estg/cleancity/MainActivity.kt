f89c999daa4abe35ca26ce3fb69544fd42471448
Gabriel <guigas78@gmail.com>
2021-03-18 18:31:42 +0000

    Merge Main Page to Master

--- CleanCity/app/src/main/java/gabriel/estg/cleancity/MainActivity.kt
+++ CleanCity/app/src/main/java/gabriel/estg/cleancity/MainActivity.kt
@@ -2,12 +2,17 @@ package gabriel.estg.cleancity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        findViewById<Button>(R.id.buttonNotes).setOnClickListener({
+            Log.i("ButtonNotes","Listener Works")
+        })
     }
 }
