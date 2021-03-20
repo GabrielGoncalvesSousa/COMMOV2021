e02ef962e322bac5f44a84378864529de055d0a5
Gabriel <guigas78@gmail.com>
2021-03-20 17:53:22 +0000

    Adding Notes Page with Date Picker

--- CleanCity/app/src/main/java/gabriel/estg/cleancity/AddNoteActivity.kt
+++ CleanCity/app/src/main/java/gabriel/estg/cleancity/AddNoteActivity.kt
@@ -1,11 +1,47 @@
 package gabriel.estg.cleancity
 
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
+import java.util.*
 
 class AddNoteActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
+
+        var toolbar = findViewById<Toolbar>(R.id.AddNoteToolbar)
+        setSupportActionBar(toolbar)
+
+        toolbar.setNavigationIcon(R.drawable.ic_action_back)
+
+        toolbar.setNavigationOnClickListener {
+            startActivity(Intent(this, NotesActivity::class.java).apply {})
+        }
+
+        var calendarView = Calendar.getInstance()
+        var year = calendarView.get(Calendar.YEAR)
+        var month = calendarView.get(Calendar.MONTH)
+        var day = calendarView.get(Calendar.DAY_OF_MONTH)
+
+        var dateView = findViewById<EditText>(R.id.editTextDate)
+        var buttonImagecalendar = findViewById<ImageView>(R.id.imageButtonCalendar)
+        buttonImagecalendar.setOnClickListener {
+            var datePicker = DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { view, mYear: Int, mMonth: Int, mDay: Int ->
+                    dateView.setText("" + mDay + "/" + (mMonth + 1) + "/" + mYear)
+                }, year, month, day
+            )
+            datePicker.show()
+        }
     }
 }
